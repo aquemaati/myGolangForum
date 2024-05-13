9dccaf2eab8255e10c0f80b4e7971309777087c4
aquemaati <aquemaati@gmail.com>
2024-05-13 23:15:51 +0200

    here i will make uuid for everyId

--- internal/controller/submitPost.go
+++ internal/controller/submitPost.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
 
 	"github.com/aquemaati/myGolangForum.git/internal/middleware"
@@ -11,7 +12,20 @@ import (
 
 func SubmitPoast(db *sql.DB, tpl *template.Template) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		index, err := UserAuthParser(r, db, w)
+		if err != nil {
+			log.Println(err)
+		}
+
+		userId := index.UserID
+
 		formData := r.Context().Value(middleware.FormDataKey).(map[string][]string)
 		fmt.Println(formData)
+
+		titlePost := formData["titlePost"][0]
+		descriptionPost := formData["descriptionPost"][0]
+		contentPost := formData["contentPost"][0]
+		category := formData["contentPost"]
+
 	}
 }
